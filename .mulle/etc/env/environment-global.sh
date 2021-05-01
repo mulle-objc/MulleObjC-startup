5870bbdc782ea18f54c8d3d9b6bb944b7c5fa521
Nat! <nat@mulle-kybernetik.com>
2021-05-01 22:59:22 +0200

    * keep version in lockstep with MulleObjC updated mulle-sde

--- .mulle/etc/env/environment-global.sh
+++ .mulle/etc/env/environment-global.sh
@@ -4,3 +4,27 @@
 export MULLE_SOURCETREE_TO_C_RUN="NO"
 
 
+#
+#
+#
+export MULLE_SOURCETREE_RESOLVE_TAG="NO"
+
+
+#
+#
+#
+export MULLE_MATCH_TO_C_RUN="NO"
+
+
+#
+#
+#
+export MULLE_MATCH_TO_CMAKE_RESOURCES_FILE="DISABLE"
+
+
+#
+#
+#
+
+
+
--- src/MulleObjC-startup.m
+++ src/MulleObjC-startup.m
@@ -48,7 +48,7 @@
 #include <stdlib.h>
 
 
-#define MULLE_OBJC_STARTUP_VERSION  ((0 << 20) | (18 << 8) | 0)
+#define MULLE_OBJC_STARTUP_VERSION  ((0 << 20) | (19 << 8) | 0)
 
 
 //
